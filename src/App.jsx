8d8ed6d67ddea8ebde730a38140f8291f7fe89f8
ThanhZung179 <dung1792000@gmail.com>
2023-06-26 17:41:25 +0700

    Done logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,87 @@
-import './App.css'
-import { useState } from 'react'
-import { DragDropContext, Draggable } from 'react-beautiful-dnd'
-import { Droppable } from 'react-beautiful-dnd'
-import _ from 'lodash'
-import { v4 } from 'uuid'
-
+import { useState } from "react";
+import "./App.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import _ from "lodash";
+import { v4 } from "uuid";
 
 const item1 = {
   id: v4(),
-  name: 'Clean the house'
-}
+  name: "Clean the house",
+};
 
 const item2 = {
   id: v4(),
-  name: 'Wash the car'
-}
+  name: "Wash the car",
+};
 
 function App() {
-
-
-
   const [state, setState] = useState({
-    'todo': {
-      title: 'Todo',
-      items: [item1]
+    todo: {
+      title: "Todo",
+      items: [item1],
     },
-    'in-progress': {
-      title: ' In progress',
-      items: [item2]
-
+    "in-progress": {
+      title: " In Progress",
+      items: [item2],
     },
-    'done': {
-      title: 'Completed',
-      items: []
-    }
-  })
+    done: {
+      title: "Completed",
+      items: [],
+    },
+  });
 
-  const handleDragEnd = (destination, source) => {
-    console.log('from', source)
-    console.log('to', destination)
+  const handleDragEnd = ({ destination, source }) => {
+    console.log("from", source);
+    console.log("to", destination);
 
     if (!destination) {
-      return
+      return;
     }
 
-    if (destination.index === source.index && destination.droppableId === source.droppableId) {
-      return
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    ) {
+      return;
     }
 
-    const itemCopy = state[source.droppableId].items[source.index]
-    console.log(itemCopy)
-
-  }
+    const itemCopy = { ...state[source.droppableId].items[source.index] };
+    setState((prev) => {
+      prev = { ...prev };
+      prev[source.droppableId].items.splice(source.index, 1);
+      prev[destination.droppableId].items.splice(
+        destination.index,
+        0,
+        itemCopy
+      );
+      return prev;
+    });
+  };
 
   return (
-    <div className='App'>
-      <DragDropContext onDragEnd={handleDragEnd} >
+    <div className="App">
+      <DragDropContext onDragEnd={handleDragEnd}>
         {_.map(state, (data, key) => {
           return (
-            <div key={key} className={'column'}>
+            <div key={key} className={"column"}>
               <h3>{data.title}</h3>
               <Droppable droppableId={key}>
                 {(provided) => (
                   <div
                     ref={provided.innerRef}
                     {...provided.droppableProps}
-                    className={'droppable-col'}
+                    className={"droppable-col"}
                   >
                     {data.items.map((el, index) => {
                       return (
-                        <Draggable key={el.id} index={index} draggableId={el.id}>
+                        <Draggable
+                          key={el.id}
+                          index={index}
+                          draggableId={el.id}
+                        >
                           {(provided) => (
                             <div
-                              className='item'
+                              className="item"
                               ref={provided.innerRef}
                               {...provided.draggableProps}
                               {...provided.dragHandleProps}
@@ -81,18 +90,18 @@ function App() {
                             </div>
                           )}
                         </Draggable>
-                      )
+                      );
                     })}
                     {provided.placeholder}
                   </div>
                 )}
               </Droppable>
             </div>
-          )
+          );
         })}
       </DragDropContext>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
